6cb7d6a9b663ed8d54bf1204d166bb943f74f9dc
Sebastian Vallejo <37245128+vallejo086@users.noreply.github.com>
2021-04-06 11:22:12 -0500

    Add files via upload

--- data_clean_results.R
+++ data_clean_results.R
@@ -35,42 +35,49 @@ resultados_df <- resultados_df[resultados_df$i!=1,]
 results_final_clean <- NA
 
 for(i in 1:length(resultados_df$mesa_temp)){
-sentences_results_junta <- char_segment(resultados_df$candidatos_temp[i],
-                                  pattern = "\n",
-                                  valuetype = "fixed",
-                                  pattern_position = "before",
-                                  remove_pattern = TRUE) 
-
-porcentaje_results_junta <- str_extract(sentences_results_junta,"[0-9]{1,2}\\,[0-9]{1,2} \\%")
-porcentaje_results_junta <- str_remove(porcentaje_results_junta, "\\%")
-porcentaje_results_junta <- str_squish(porcentaje_results_junta)
-
-sentences_results_junta <- str_remove(sentences_results_junta,"[0-9]{1,2}\\,[0-9]{1,2} \\%")
-sentences_results_junta <- str_squish(sentences_results_junta)
-
-total_results_junta <- str_extract(sentences_results_junta,"[0-9]{1,5}$")
-
-party <- str_remove(sentences_results_junta,"[0-9]{1,5}$")
-party <- str_squish(party)
-
-mesa_temp <- paste(resultados_df$mesa_temp[i], " >", sep = "")
-sentences_loc_junta <- char_segment(mesa_temp,
-                                        pattern = ">",
-                                        valuetype = "fixed",
-                                        pattern_position = "after",
-                                        remove_pattern = TRUE) 
-
-
-if(str_detect(sentences_loc_junta[2],"CIRCUNSCRIPCIÓN")){
-results_temp <- cbind.data.frame(party,total_results_junta,porcentaje_results_junta,sentences_loc_junta[1],sentences_loc_junta[2],sentences_loc_junta[3],sentences_loc_junta[4],sentences_loc_junta[5],sentences_loc_junta[6])
-colnames(results_temp) <- c("partido", "total_votos_junta", "porcentaje_votos_junta", "provincia", "circunscripcion","canton","parroquia","zona","junta")
-results_final_clean <- rbind.data.frame(results_final_clean,results_temp)
-}else{
-  results_temp <- cbind.data.frame(party,total_results_junta,porcentaje_results_junta,sentences_loc_junta[1],"",sentences_loc_junta[2],sentences_loc_junta[3],sentences_loc_junta[4],sentences_loc_junta[5])
-  colnames(results_temp) <- c("partido", "total_votos_junta", "porcentaje_votos_junta", "provincia", "circunscripcion","canton","parroquia","zona","junta")
-  results_final_clean <- rbind.data.frame(results_final_clean,results_temp)
-}
-print(i)
+  
+  # Use \n as a separator 
+  sentences_results_junta <- char_segment(resultados_df$candidatos_temp[i],
+                                          pattern = "\n",
+                                          valuetype = "fixed",
+                                          pattern_position = "before",
+                                          remove_pattern = TRUE) 
+  
+  # extract the % votes by using the pattern "[0-9]{1,2}\\,[0-9]{1,2} \\%"
+  porcentaje_results_junta <- str_extract(sentences_results_junta,"[0-9]{1,2}\\,[0-9]{1,2} \\%")
+  porcentaje_results_junta <- str_remove(porcentaje_results_junta, "\\%") # Clean
+  porcentaje_results_junta <- str_squish(porcentaje_results_junta) # Clean
+  
+  # Remore the % votes from the original character string to now extract total number of votes
+  sentences_results_junta <- str_remove(sentences_results_junta,"[0-9]{1,2}\\,[0-9]{1,2} \\%")
+  sentences_results_junta <- str_squish(sentences_results_junta)
+  
+  # extract total number of votes by using the pattern "[0-9]{1,5}$"
+  total_results_junta <- str_extract(sentences_results_junta,"[0-9]{1,5}$")
+  
+  # extract party name by removing the number of votes
+  party <- str_remove(sentences_results_junta,"[0-9]{1,5}$")
+  party <- str_squish(party)
+  
+  # Separate location by using the marker ">"
+  mesa_temp <- paste(resultados_df$mesa_temp[i], " >", sep = "")
+  sentences_loc_junta <- char_segment(mesa_temp,
+                                      pattern = ">",
+                                      valuetype = "fixed",
+                                      pattern_position = "after",
+                                      remove_pattern = TRUE) 
+  
+  # Some location do not have circunscripcion so I account for that 
+  if(str_detect(sentences_loc_junta[2],"CIRCUNSCRIPCIÓN")){
+    results_temp <- cbind.data.frame(party,total_results_junta,porcentaje_results_junta,sentences_loc_junta[1],sentences_loc_junta[2],sentences_loc_junta[3],sentences_loc_junta[4],sentences_loc_junta[5],sentences_loc_junta[6])
+    colnames(results_temp) <- c("partido", "total_votos_junta", "porcentaje_votos_junta", "provincia", "circunscripcion","canton","parroquia","zona","junta")
+    results_final_clean <- rbind.data.frame(results_final_clean,results_temp)
+  }else{
+    results_temp <- cbind.data.frame(party,total_results_junta,porcentaje_results_junta,sentences_loc_junta[1],"",sentences_loc_junta[2],sentences_loc_junta[3],sentences_loc_junta[4],sentences_loc_junta[5])
+    colnames(results_temp) <- c("partido", "total_votos_junta", "porcentaje_votos_junta", "provincia", "circunscripcion","canton","parroquia","zona","junta")
+    results_final_clean <- rbind.data.frame(results_final_clean,results_temp)
+  }
+  print(i)
 }
 
 # Dusting
